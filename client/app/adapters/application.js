0e84290a53d9bfacdf987f0761697c553d3f7f71
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-04-22 15:02:05 +0100

    adapter changed to REST from Fixture for persistent data

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,4 +1,12 @@
+// old Fixture serving static data no longer needed
+// import DS from 'ember-data';
+
+// export default DS.FixtureAdapter.extend({
+// });
+
+// new persistent data
 import DS from 'ember-data';
 
-export default DS.FixtureAdapter.extend({
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:1337'
 });
